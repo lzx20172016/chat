ed783e0ac10221bdc1f4af34b3ac7b34debdd81c
lzx <2295146798@qq.com>
2017-06-24 10:37:43 -0400

    init

---
+++ chatdef.h
@@ -0,0 +1,10 @@
+#ifndef CHATDEF_H
+#define CHATDEF_H
+#define BOARDCAST "255.255.255.255"
+#define CMD "cmd"
+#define NAME "name"
+#define ONLINE "online"
+#define ONLINEACK "onlineack"
+
+
+#endif // CHATDEF_H
